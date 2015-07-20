f39aa20c2db156d5708cdb53c920555ce560af07
cmgraff <cm0graff@gmail.com>
2015-07-20 11:12:09 -0500

    build.sh uploaded to ask developer advice on uclibc and newlibc

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-#set -ex
+set -ex
 
 # aarch64
 	#MYTARG="aarch64-linux"
 	#MYLINUXARCH="arm64"
 
 # i586
-	MYTARG="i586-linux"
-	MYLINUXARCH="x86"
+	#MYTARG="i586-linux"
+	#MYLINUXARCH="x86"
 
 # x86_64
-	#MYTARG="x86_64-linux"
-	#MYLINUXARCH="x86_64" 
+	MYTARG="x86_64-linux"
+	MYLINUXARCH="x86_64" 
 
 MYPREF="$(pwd)/toolchain/"
 MYSRC="$(pwd)/src" 
@@ -22,7 +22,7 @@ MYGMP="gmp-6.0.0"
 MYMPC="mpc-1.0.2"
 MYMPFR="mpfr-3.1.2" 
 MYSTARTDIR="$(pwd)"
-MYJOBS="-j8"
+MYJOBS="-j4"
 #MYLANGS="c,c++" 
 MYLANGS="c"
 MYLINUX="kernel-headers-3.12.6-5" 
@@ -30,6 +30,8 @@ MYCONF="--disable-multilib --with-multilib-list="
 #MYCONF="--with-multilib-list=mx32"
 MYMUSL="musl-1.1.6"
 MYGLIBC="glibc-2.20" 
+MYNEWLIB="newlib-2.2.0"
+MYUCLIBC="uClibc-ng-1.0.4"
 SUFFIX="tar.xz"
 
 
@@ -64,10 +66,11 @@ common_obtain_source_code()
 common_clean()
 {
         rm -rf ${MYBINUTILS} ${MYGCC} ${MYGLIBC} \
-         ${MYLINUX} ${MYMPC} ${MYMPFR} ${MYPREF} \
+         ${MYLINUX} ${MYMPC} ${MYMPFR} ${MYPREF} ${MYNEWLIB} \
         build-glibc build-binutils build-gcc gmp-6.0.0 isl gmp \
         cloog mpc mpfr a.out build-newlib newlib-master logfile.txt
-        rm -rf ${MYMUSL}
+        rm -rf ${MYMUSL} ${MYUCLIBC}
+	rm -rf build-uclibc
         rm -rf toolchain/
         rm -rf build-binutils/
         rm -rf musl-build/
@@ -75,7 +78,7 @@ common_clean()
         rm -rf build2-gcc
         rm -rf a.out
 }
-common_clean
+common_clean 
 
 
 common_binutils_stage()
@@ -107,9 +110,11 @@ common_linux_stage
 common_gcc_stage_one()
 {
         tar -xf "${MYSRC}/${MYGCC}.${SUFFIX}"
-        cd "${MYGCC}"
-        patch -p1 < "${MYSTARTDIR}/patches/${MYGCC}-musl.diff"
-        cd "${MYSTARTDIR}"
+
+	# this patch is only needed for musl
+        #cd "${MYGCC}"
+        #patch -p1 < "${MYSTARTDIR}/patches/${MYGCC}-musl.diff"
+        #cd "${MYSTARTDIR}"
 
         tar -xf "${MYSRC}/${MYGMP}.${SUFFIX}"
         tar -xf "${MYSRC}/${MYMPFR}.${SUFFIX}"
@@ -187,9 +192,36 @@ glibc_stage()
 	make install 
 	cd "${MYSTARTDIR}"
 }
-# glibc_stage
+#glibc_stage
 
+newlib_stage()
+{
+	tar -xf "${MYSRC}/${MYNEWLIB}.${SUFFIX}"
+	mkdir -p build-newlib
+	cd build-newlib 
+	${MYSTARTDIR}/${MYNEWLIB}/configure \
+        --target="${MYTARG}" \
+        --prefix="${MYPREF}" \
+	--disable-threads \
+	--disable-shared
+	#$MYCONF 
+	make "${MYJOBS}"
+	make install
+	cd "${MYSTARTDIR}" 
+        #mkdir build2-gcc
+        cd build2-gcc
+        ${MYSTARTDIR}/${MYGCC}/configure \
+        --prefix="$MYPREF" \
+        --target=${MYTARG} \
+        --enable-languages=${MYLANGS} \
+	--disable-threads 
+        make "${MYJOBS}"
+        make install
 
+        cd "${MYSTARTDIR}" 
+
+}
+#newlib_stage
 musl_stage()
 {
 
@@ -220,7 +252,8 @@ musl_stage()
         --target=${MYTARG} \
         --enable-languages=${MYLANGS} \
         --disable-libmudflap \
-        --disable-libsanitizer --disable-nls \
+        --disable-libsanitizer \
+	--disable-nls \
         $MYCONF
 
         make "${MYJOBS}"
@@ -229,4 +262,55 @@ musl_stage()
         cd "${MYSTARTDIR}"
 }
 musl_stage
+
+uclibc_stage()
+{
+
+        # musl
+        tar -xf "${MYSRC}/${MYUCLIBC}.${SUFFIX}"
+
+	#mkdir build-uclibc
+	#cd build-uclibc
+	cd ${MYUCLIBC}
+        
+        #${MYSTARTDIR}/${MYUCLIBC}/configure \
+        #--prefix="${MYPREF}/${MYTARG}" \
+        #--enable-debug \
+        #--enable-optimize \
+        #CROSS_COMPILE="${MYTARG}-" CC="${MYTARG}-gcc"
+	#make defconfig
+	make menuconfig
+
+       	#KERNEL_SOURCE="${MYPREF}/${MYTARG}" \
+	#TARGET_ARCH=x86_64 PREFIX="${MYPREF}/${MYTARG}" \
+	#CROSS_COMPILE="${MYTARG}-" \
+	#CC="${MYTARG}-gcc" \
+	#make "${MYJOBS}"
+
+        make install
+
+        cd "${MYSTARTDIR}"
+
+        # gcc 2
+        if [ ! -e "$MYPREF/${MYTARG}/lib/libc.so" ]
+        then    MYCONF="${MYCONF} --disable-shared "
+        fi
+
+        mkdir build2-gcc
+        cd build2-gcc
+        ${MYSTARTDIR}/${MYGCC}/configure \
+        --prefix="$MYPREF" \
+        --target=${MYTARG} \
+        --enable-languages=${MYLANGS} \
+        --disable-libmudflap \
+        --disable-libsanitizer \
+        --disable-nls \
+        $MYCONF
+
+        make "${MYJOBS}"
+        make install
+
+        cd "${MYSTARTDIR}"
+}
+#uclibc_stage
 
